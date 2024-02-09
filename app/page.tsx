1760c284526e1f9594cbfa9c7575119a1d4034e3
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-02-09 23:55:56 +0100

    feat: modify index to cover our needs

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,40 @@
-import Image from "next/image";
+"use client";
+import ItemWrapper from "@/components/basic/ItemWrapper";
+import Ratings from "@/components/ratings/Ratings";
+import { useEffect, useState } from "react";
+import { getAllProductsBasicInformation } from "@/lib/products";
 import styles from "./page.module.css";
 
+const createProductHref = (id: string) => `/products/${id}`;
+const createProductImgSrc = (id: string) => `/products/${id}.webp`;
+
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getAllProductsBasicInformation().then((products) => setProducts(products));
+  }, []);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className={styles.ratingsContainer}>
+        <Ratings
+          title={"Custom stickers"}
+          titleStyle="huge"
+          rating={4.5}
+          reviews={20000}
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.productsContainer}>
+        {products.map(({ id, name }, index) => (
+          <ItemWrapper
+            key={index}
+            href={createProductHref(id)}
+            imgAlt={name}
+            imgSrc={createProductImgSrc(id)}
+            title={name}
+          />
+        ))}
       </div>
     </main>
   );
