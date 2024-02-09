a6b506b77a1d993f9498802bd7767d4df781cd08
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-02-09 23:50:35 +0100

    feat: add lib to fetch graphql data

---
+++ lib/products.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3000/?query=";
+
+export const getAllProductsBasicInformation = () => {
+  const query = encodeURIComponent("{allProducts{id,name}}");
+  return axios
+    .get(`${baseUrl}${query}`)
+    .then((response) => response.data)
+    .then((data) => data.data.allProducts)
+    .catch((error) => console.log(error));
+};
+export const getProductInformationById = (id: string) => {
+  const query = encodeURIComponent(
+    `{Product(id:"${id}"){name,description,rating,reviews,sizeOptions,quantityOptions}}`
+  );
+  return axios
+    .get(`${baseUrl}${query}`)
+    .then((response) => response.data)
+    .then((data) => data.data.Product)
+    .catch((error) => console.log(error));
+};
