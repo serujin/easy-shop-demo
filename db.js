5443aa96dad9a559788a578531b857e57ca75328
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-02-09 23:47:46 +0100

    feat: add mock database as alternative to backend

---
+++ db.js
@@ -0,0 +1,197 @@
+module.exports = {
+  cart: [{ products_id: "", quantity: 0 }],
+  products: [
+    {
+      id: "die-cut-stickers",
+      description:
+        "Custom die cut stickers are a fast and easy way to promote your business, brand or event. Thick, durable vinyl protects your stickers from scratches, water & sunlight.",
+      name: "Die cut stickers",
+      rating: 4.5,
+      reviews: 76746,
+      sizeOptions: [
+        { value: "2″ x 2″" },
+        { value: "3″ x 3″" },
+        { value: "4″ x 4″" },
+        { value: "5″ x 5″" },
+        { value: "Custom size" },
+      ],
+      quantityOptions: [
+        { value: "50", price: { quantity: 60, currency: "€" } },
+        { value: "100", price: { quantity: 73, currency: "€" }, discount: 39 },
+        { value: "200", price: { quantity: 95, currency: "€" }, discount: 60 },
+        { value: "300", price: { quantity: 115, currency: "€" }, discount: 68 },
+        { value: "500", price: { quantity: 152, currency: "€" }, discount: 75 },
+        {
+          value: "1000",
+          price: { quantity: 232, currency: "€" },
+          discount: 81,
+        },
+        {
+          value: "2000",
+          price: { quantity: 371, currency: "€" },
+          discount: 85,
+        },
+        {
+          value: "3000",
+          price: { quantity: 496, currency: "€" },
+          discount: 86,
+        },
+        {
+          value: "5000",
+          price: { quantity: 723, currency: "€" },
+          discount: 88,
+        },
+        {
+          value: "10000",
+          price: { quantity: 1225, currency: "€" },
+          discount: 90,
+        },
+        { value: "Custom quantity" },
+      ],
+    },
+    {
+      id: "circle-stickers",
+      description:
+        "Custom circle stickers are a fast and easy way to promote your business, brand or event. Thick, durable vinyl protects your stickers from scratches, water and sunlight.",
+      name: "Circle stickers",
+      rating: 4.7,
+      reviews: 22333,
+      sizeOptions: [
+        { value: "1″ x 1″" },
+        { value: "2″ x 2″" },
+        { value: "3″ x 3″" },
+        { value: "4″ x 4″" },
+        { value: "Custom size" },
+      ],
+      quantityOptions: [
+        { value: "50", price: { quantity: 51, currency: "€" } },
+        { value: "100", price: { quantity: 57, currency: "€" }, discount: 44 },
+        { value: "200", price: { quantity: 67, currency: "€" }, discount: 67 },
+        { value: "300", price: { quantity: 75, currency: "€" }, discount: 75 },
+        { value: "500", price: { quantity: 90, currency: "€" }, discount: 82 },
+        {
+          value: "1000",
+          price: { quantity: 122, currency: "€" },
+          discount: 88,
+        },
+        {
+          value: "2000",
+          price: { quantity: 175, currency: "€" },
+          discount: 91,
+        },
+        {
+          value: "3000",
+          price: { quantity: 223, currency: "€" },
+          discount: 93,
+        },
+        {
+          value: "5000",
+          price: { quantity: 308, currency: "€" },
+          discount: 94,
+        },
+        {
+          value: "10000",
+          price: { quantity: 491, currency: "€" },
+          discount: 95,
+        },
+        { value: "Custom quantity" },
+      ],
+    },
+    {
+      id: "rectangle-stickers",
+      description:
+        "Custom rectangle stickers are a fast and easy way to promote your business, brand or event. Thick, durable vinyl protects your stickers from scratches, water & sunlight.",
+      name: "Rectangle stickers",
+      rating: 4.6,
+      reviews: 5970,
+      sizeOptions: [
+        { value: "2″ x 1″" },
+        { value: "3″ x 2″" },
+        { value: "4″ x 2″" },
+        { value: "5″ x 3″" },
+        { value: "Custom size" },
+      ],
+      quantityOptions: [
+        { value: "50", price: { quantity: 54, currency: "€" } },
+        { value: "100", price: { quantity: 61, currency: "€" }, discount: 44 },
+        { value: "200", price: { quantity: 74, currency: "€" }, discount: 66 },
+        { value: "300", price: { quantity: 85, currency: "€" }, discount: 74 },
+        { value: "500", price: { quantity: 106, currency: "€" }, discount: 80 },
+        {
+          value: "1000",
+          price: { quantity: 150, currency: "€" },
+          discount: 86,
+        },
+        {
+          value: "2000",
+          price: { quantity: 226, currency: "€" },
+          discount: 90,
+        },
+        {
+          value: "3000",
+          price: { quantity: 293, currency: "€" },
+          discount: 91,
+        },
+        {
+          value: "5000",
+          price: { quantity: 415, currency: "€" },
+          discount: 92,
+        },
+        {
+          value: "10000",
+          price: { quantity: 679, currency: "€" },
+          discount: 94,
+        },
+        { value: "Custom quantity" },
+      ],
+    },
+    {
+      id: "square-stickers",
+      description:
+        "Custom square stickers are a fast and easy way to promote your business, brand or event. Thick, durable vinyl protects your stickers from scratches, water & sunlight.",
+      name: "Square stickers",
+      rating: 5,
+      reviews: 3366,
+      sizeOptions: [
+        { value: "2″ x 2″" },
+        { value: "3″ x 3″" },
+        { value: "4″ x 4″" },
+        { value: "5″ x 5″" },
+        { value: "Custom size" },
+      ],
+      quantityOptions: [
+        { value: "50", price: { quantity: 58, currency: "€" } },
+        { value: "100", price: { quantity: 69, currency: "€" }, discount: 41 },
+        { value: "200", price: { quantity: 88, currency: "€" }, discount: 62 },
+        { value: "300", price: { quantity: 104, currency: "€" }, discount: 70 },
+        { value: "500", price: { quantity: 134, currency: "€" }, discount: 77 },
+        {
+          value: "1000",
+          price: { quantity: 200, currency: "€" },
+          discount: 83,
+        },
+        {
+          value: "2000",
+          price: { quantity: 314, currency: "€" },
+          discount: 86,
+        },
+        {
+          value: "3000",
+          price: { quantity: 417, currency: "€" },
+          discount: 88,
+        },
+        {
+          value: "5000",
+          price: { quantity: 603, currency: "€" },
+          discount: 90,
+        },
+        {
+          value: "10000",
+          price: { quantity: 1011, currency: "€" },
+          discount: 91,
+        },
+        { value: "Custom quantity" },
+      ],
+    },
+  ],
+};
