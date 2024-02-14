0e2e060f1383c200d1eaa225dc35811ba7535087
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-02-14 22:07:56 +0100

    fix: make star display component work correctly

--- components/ratings/star-display/StarDisplay.tsx
+++ components/ratings/star-display/StarDisplay.tsx
@@ -10,9 +10,11 @@ interface StarDisplayProps {
 const MAX_RATING = 5
 
 const getStars = (rating: number) => {
-  const fullStars = rating ? Math.floor(rating) : 0
-  const emptyStars = MAX_RATING - fullStars
-  const shouldShowHalfStar = rating - fullStars >= 0.5
+  const roundedRating = rating ? Math.round(rating * 2) / 2 : 0
+  const fullStars = Math.floor(roundedRating)
+  const shouldShowHalfStar = roundedRating - fullStars === 0.5
+  const emptyStars =
+    MAX_RATING - (shouldShowHalfStar ? fullStars + 1 : fullStars)
   return { emptyStars, fullStars, shouldShowHalfStar }
 }
 
