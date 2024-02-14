05f777b27b89ea8ba4a4b3803ef8ef331fbd0263
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-02-14 20:46:26 +0100

    refactor: change path for jest snapshots

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  snapshotResolver: "<rootDir>/snapshotResolver.js",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
 };
