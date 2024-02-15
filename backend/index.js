5b8998abb71b325b2b8be827e41c1d406a9e9bd2
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-02-16 00:01:30 +0100

    feat(backend): cleanup

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,11 @@ const express = require("express");
 const jsonGraphqlExpress = require("json-graphql-server");
 const cors = require("cors");
 
-const logger = (req, _, next) => {
-  console.log(req.body);
-  console.log(req.method);
-  next();
-};
-
 const PORT = 3000;
 const app = express();
 const data = require("./database.json");
 
 app.use(cors());
-app.use(logger);
 app.use("/", jsonGraphqlExpress.default(data));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
