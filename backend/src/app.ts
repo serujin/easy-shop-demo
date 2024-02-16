4398e3cef52382fde8009647041ba332bbd58add
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-02-16 17:00:50 +0100

    feat(backend): add typescript

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import jsonGraphqlExpress from "json-graphql-server";
+import cors from "cors";
+import data from "../database.json";
+
+const app = express();
+
+app.use(cors());
+app.use("/", jsonGraphqlExpress(data));
+
+export default app;
---
+++ backend/src/main.ts
@@ -0,0 +1,6 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT;
+app.listen(PORT);
